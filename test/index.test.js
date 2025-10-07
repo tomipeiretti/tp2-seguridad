88a04e5d18a3d9176774418f01951f2b2b8fb271
Tomas <jtomaspeiretti@gmail.com>
2025-10-07 01:54:33 -0300

    Cambios en el test

--- test/index.test.js
+++ test/index.test.js
@@ -6,20 +6,8 @@ test('La función getWeather debe estar definida', () => {
   expect(typeof getWeather).toBe('function');
 });
 
-// Test 2: que lance error si falta la API_KEY
-test('Debe lanzar error si falta la variable de entorno API_KEY', async () => {
-  delete process.env.API_KEY; // eliminamos temporalmente la variable
-  await expect(getWeather('Mendoza')).rejects.toThrow('No se encontró la variable de entorno API_KEY');
+test("La función getWeather devuelve un string", async () => {
+  const result = await getWeather("Mendoza");
+  expect(typeof result).toBe("string");
 });
-
-// Test 3 (opcional): que devuelva datos válidos si la API_KEY está definida
-//  Este test solo funcionará si tenés una API_KEY real cargada
-/*
-test('Debe devolver datos del clima para Mendoza', async () => {
-  process.env.API_KEY = 'tu_api_key_real_aqui';
-  const data = await getWeather('Mendoza');
-  expect(data).toHaveProperty('main.temp');
-  expect(data).toHaveProperty('weather');
-});
-*/
 
